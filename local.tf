70954fb2430358ddf62f37ac5cf1d61e0ffc1d30
SrinathDevOps <srinath.kaithoju@gmail.com>
2022-12-19 18:54:53 +0530

    new file added
    Signed-off-by: SrinathDevOps <srinath.kaithoju@gmail.com>

---
+++ local.tf
@@ -0,0 +1,15 @@
+resource "local_file" "pet" {
+    filename = "/root/pet.txt"
+    content= "we loves pets"
+  
+ /* #<block> <parameters>{
+key1 =value1
+key2 =value2
+local_file resource type
+local=provider
+file-resource
+resource name=pet
+arguments are filename and content
+} 
+*/
+}
