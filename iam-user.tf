b8678465bcee07a06ec7dc90926ae7cc8df49b78
Srinath <72057855+SrinathDevOps@users.noreply.github.com>
2022-12-23 20:11:59 +0530

    Update iam-user.tf

--- iam-user.tf
+++ iam-user.tf
@@ -1,7 +1,7 @@
 provider "aws"{
    region = "us-east-1"
-   access_key = "AKIASJ5GJWGPQT7SB6L4"  
-   secret_key = "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"
+   access_key = "AKIASJ5GJWGPQT7SggggB6L4"  
+   secret_key = "aoEUku+5s1YoC/gggggg"
 }
 resource "aws_iam_user" "users" {
    name= "Srips" 
