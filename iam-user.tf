2733b5a076e1fe6a1542f96809f460d3944948e5
SrinathDevOps <srinath.kaithoju@gmail.com>
2022-12-23 13:56:21 +0530

    added the new creds
    Signed-off-by: SrinathDevOps <srinath.kaithoju@gmail.com>

--- iam-user.tf
+++ iam-user.tf
@@ -1,3 +1,11 @@
+provider "aws"{
+   region = "us-east-1"
+   access_key = "AKIASJ5GJWGPQT7SB6L4"  
+   secret_key = "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"
+}
 resource "aws_iam_user" "users" {
-   name= "mary" 
+   name= "Srips" 
+   tags = {
+      name ="demo test user"
+   }
   }
